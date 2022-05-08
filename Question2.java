144cc3e95aca3aef22b357f381caaa11bf96b92c
Lairton da Silva <lairtondasilva07@gmail.com>
2022-05-08 14:22:20 -0300

    Questao 2 finalizada

---
+++ Question2.java
@@ -0,0 +1,36 @@
+package condicao;
+
+import java.util.Scanner;
+
+public class Question2 {
+	public static void main(String[]args) {
+		Scanner reader = new Scanner(System.in);
+		int a,b,c;
+		
+		System.out.println("digite o valor de a: ");
+		a = reader.nextInt();
+		System.out.println("digite o valor de b: ");
+		b = reader.nextInt();
+		System.out.println("digite o valor de c: ");
+		c = reader.nextInt();
+		
+		if(a<=b && b<=c) {
+			System.out.println(a+" , "+b+" , "+c);
+		}
+		else if(a<=c && c<=b) {
+			System.out.println(a+" , "+c+" , "+b);
+		}
+		else if(b<=a && a<=c) {
+			System.out.println(b+" , "+a+" , "+c);
+		}
+		else if(b<=c && c<=a) {
+			System.out.println(b+" , "+c+" , "+a);
+		}
+		else if(c<=a && a<=b) {
+			System.out.println(c+" , "+a+" , "+b);
+		}
+		else {
+			System.out.println(c+" , "+b+" , "+a);
+		}
+	}
+}
